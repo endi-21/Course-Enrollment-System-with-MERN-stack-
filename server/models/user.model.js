dd937f539d1ba179cacf339e22193bc5f7407547
Endi Brahja <endibrahja@gmail.com>
2024-11-03 22:43:28 +0100

    Create user.model.js

---
+++ server/models/user.model.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        role: {
+            type: String,
+            required: true,
+        },
+		profilePic: {
+			type: String, //url of a picture 
+			required: true,
+            default: null,
+		},
+	},
+	{
+		timestamps: true, // creation and update time
+	}
+);
+
+const User = mongoose.model("User", userSchema);
+						  //model name 
+export default User;
