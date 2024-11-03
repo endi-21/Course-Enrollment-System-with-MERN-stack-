fdd22e753a4ccd8ad3bbef5338ec3aa4005c0c69
Endi Brahja <endibrahja@gmail.com>
2024-11-03 23:51:15 +0100

    Create course.model.js

---
+++ server/models/course.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const courseSchema = new mongoose.Schema(
+	{
+		title: {
+			type: String,
+			required: true,
+		},
+		description: {
+			type: String,
+			required: true,
+		},
+		instructor_id: {
+			type: mongoose.Schema.Types.ObjectId, // reference to the User model
+			ref: "User", 
+			required: true,
+		},
+        student_ids: [{ 
+            type: mongoose.Schema.Types.ObjectId, 
+            ref: "User", 
+        }],
+        
+	},
+	{
+		timestamps: true, // creation and update time
+	}
+);
+
+const Course = mongoose.model("Course", courseSchema);
+						    //model name 
+export default Course;
