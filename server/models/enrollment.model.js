4c0329088f69e3626b1393213fd7fbb1f02314fe
Endi Brahja <endibrahja@gmail.com>
2024-11-03 23:57:51 +0100

    Create enrollment.model.js

---
+++ server/models/enrollment.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const enrollmentSchema = new mongoose.Schema(
+    {
+        student_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: "User", // Reference to the User model
+        },
+        course_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: "Course", // Reference to the Course model
+        },
+        start_date: {
+            type: Date,
+            required: true,
+        },
+        end_date: {
+            type: Date,  // when students finishes the course 
+        },
+    },
+    
+);
+
+const Enrollment = mongoose.model("Enrollment", enrollmentSchema);
+export default Enrollment;
