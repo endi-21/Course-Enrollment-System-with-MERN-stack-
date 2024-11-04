4ee61eb6c4476ae23663d1637bee1f3b50d72d7c
Endi Brahja <endibrahja@gmail.com>
2024-11-04 23:11:43 +0100

    Create user.route.js
    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/database.js";
-import User from "./models/user.model.js";
+import userRoutes from "./routes/user.route.js";
 
 dotenv.config(); 
 
@@ -9,23 +9,7 @@ const app = express();
 
 app.use(express.json());  //to get JSON data in req.body
 
-app.post("/api/users", async (req, res) => {
-    const user = req.body;
-
-    if(!user.name || !user.email || !user.password || !user.role){
-        return res.status(400).json({success: false, message: "Please provide all fields"});
-    }
-
-    const newUser = new User(user);
-
-    try{
-        await newUser.save(); //mongoose function to save data to db 
-        return res.status(201).json({success: true, data: newUser});
-    } catch(error){
-        console.log("Error:", error.message);
-        return res.status(500).json({success: false, message: "Server error"}) 
-    }
-});
+app.use("/api/users", userRoutes); 
 
 
 console.log(process.env.MONGO_URI); 
---
+++ server/routes/user.route.js
@@ -0,0 +1,26 @@
+import express from "express";
+import mongoose from 'mongoose';
+import User from "../models/user.model.js";
+
+const router = express.Router(); 
+
+router.post("/", async (req, res) => {
+    const user = req.body;
+
+    if(!user.name || !user.email || !user.password || !user.role){
+        return res.status(400).json({success: false, message: "Please provide all fields"});
+    }
+
+    const newUser = new User(user);
+
+    try{
+        await newUser.save(); //mongoose function to save data to db 
+        return res.status(201).json({success: true, data: newUser});
+    } catch(error){
+        console.log("Error:", error.message);
+        return res.status(500).json({success: false, message: "Server error"}) 
+    }
+});
+
+
+export default router; 
