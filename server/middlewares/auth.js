ad925b8912ad68f7acfc6c46f7ecb229a51ec3ff
Endi Brahja <endibrahja@gmail.com>
2024-11-14 13:25:25 +0100

    Add authorization check for admin in auth.js

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -11,6 +11,15 @@ export const protect = async (req, res, next) => {
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = await User.findById(decoded.id).select("-password");
+
+        if ( //if req is coming from neither admin nor the user who is owner, return 403
+            req.user.role !== "admin" && req.params.id !== req.user.id &&
+            (req.method === "POST" || req.method === "PUT" || req.method === "DELETE") &&
+            (req.path.includes("/users/") || req.path.includes("/courses/") || req.path.includes("/enrollments/"))
+        ) {
+            return res.status(403).json({ success: false, message: "Not authorized" });
+        }
+
         next();
     } catch (error) {
         res.status(401).json({ success: false, message: "Not authorized, token invalid" });
