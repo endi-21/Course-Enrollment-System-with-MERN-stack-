4dfa42da654f1522d59a60a3fece5f1065e7f514
Endi Brahja <endibrahja@gmail.com>
2024-11-18 23:56:48 +0100

    Update updateEnrollment method

--- server/controllers/enrollment.controller.js
+++ server/controllers/enrollment.controller.js
@@ -56,10 +56,6 @@ export const updateEnrollment = async (req, res) => {
     const {id} = req.params;
     const enrollment = req.body;
 
-    if((checkEnrollmentFields(enrollment))){
-        return res.status(400).json({success: false, message: "Please provide all fields"});
-    }
-
     if (!mongoose.Types.ObjectId.isValid(id)) {
         return res.status(404).json({ success: false, message: "Invalid Id" });
     }
