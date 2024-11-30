1ce08ed568f48071e365aab25e4b34b365f12287
Endi Brahja <endibrahja@gmail.com>
2024-11-30 04:48:12 +0100

    Add protect middleware for setEnrollmentEndDate method

--- server/routes/enrollment.route.js
+++ server/routes/enrollment.route.js
@@ -7,11 +7,11 @@ const router = express.Router();
 router.get("/", getAllEnrollments);
 router.get("/:id", getEnrollmentById);
 router.get("/:studentId/:courseId", getEnrollmentByStudentAndCourseId);
-router.put("/:id/end-date", setEnrollmentEndDate);
 
 //protect middleware
 router.post("/", protect, createEnrollment); 
 router.put("/:id", protect, updateEnrollment); 
 router.delete("/:id", protect, deleteEnrollment); 
+router.put("/:id/end-date", protect, setEnrollmentEndDate);
 
 export default router;
