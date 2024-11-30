0493b6023ebf7a0d5b2340883d8131fbadee2fa6
Endi Brahja <endibrahja@gmail.com>
2024-11-30 14:31:08 +0100

    Fetch courses by instructor id

--- client/src/components/InstructorCourses.jsx
+++ client/src/components/InstructorCourses.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import CourseCard from './UI/CourseCard';
+import axios from 'axios';
 
 const InstructorCourses = () => {
-  return (
-    <div>InstructorCourses</div>
-  )
-}
+    const [courses, setCourses] = useState([]);
+    const [error, setError] = useState(null);
 
-export default InstructorCourses
+    useEffect(() => {
+        const fetchCourses = async () => {
+            const user = JSON.parse(localStorage.getItem('user'));
+            if (!user || !user.data?.user?._id) {
+                setError('User information is missing or invalid');
+                return;
+            }
+
+            try {
+                const courseResponse = await axios.get(
+                    `http://localhost:5000/api/courses/instructor/${user.data.user._id}`
+                );
+
+                setCourses(courseResponse.data.data); 
+            } catch (err) {
+                setError(err.response?.data?.error || err.message);
+            }
+        };
+
+        fetchCourses();
+    }, []);
+
+    if (!courses.length) {
+        return <div>You have not enrolled any courses yet!</div>;
+    } else if (error) {
+        return <div>Error: {error}</div>;
+    }
+
+    return (
+        <div>
+            <h2>Enrolled Courses</h2>
+            <div>
+                {courses.map((course) => (
+                    <CourseCard
+                        key={course._id}
+                        courseData={course}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default InstructorCourses;
