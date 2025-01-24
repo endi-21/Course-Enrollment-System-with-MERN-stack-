d988499993e7dbe17c1dedaf771e4bc48e037bf0
Endi Brahja <endibrahja@gmail.com>
2025-01-25 00:59:26 +0100

    Return more user data in jwt

--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -2,7 +2,14 @@ import jwt from "jsonwebtoken";
 
 export const generateToken = (user) => {
     return jwt.sign(
-        { id: user._id, role: user.role },
+        { 
+            id: user._id, 
+            name: user.name,
+            email: user.email,
+            role: user.role,
+            profilePic: user.profilePic,
+            description: user.description
+        },
         process.env.JWT_SECRET,
         { expiresIn: "1d" } // expires in 1 day
     );
